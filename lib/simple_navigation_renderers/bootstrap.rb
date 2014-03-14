ffbfd952c294bc70cf0592e5d453200acec8de87
Pavel Shpak <shpakvel@gmail.com>
2014-03-14 13:55:07 +0300

    correct name_generator according to new simple-navigation

--- lib/simple_navigation_renderers/bootstrap.rb
+++ lib/simple_navigation_renderers/bootstrap.rb
@@ -6,51 +6,47 @@ module SimpleNavigationRenderers
       if (skip_if_empty? && item_container.empty?)
         ''
       else
-        config_selected_class = SimpleNavigation.config.selected_class
-        SimpleNavigation.config.selected_class = "active"
-        @config_name_generator = SimpleNavigation.config.name_generator
-        SimpleNavigation.config.name_generator = self.method(:name_generator)
-        
-        list_content = item_container.items.inject([]) do |list, item|
-          li_options = item.html_options
+        list_content = with_bootstrap_configs do
 
-          navbar_text = li_options.delete(:navbar_text)
-          navbar_divider = li_options.delete(:divider)
-          navbar_header = li_options.delete(:header)
-          link_options = li_options.delete(:link) || {}
-          split = li_options.delete(:split)
+          item_container.items.inject([]) do |list, item|
+            li_options = item.html_options
 
-          if navbar_text
-            list << li_text( item.name, li_options )
-          elsif navbar_divider
-            list << li_divider( item_container.level, li_options )
-          elsif navbar_header && (item_container.level != 1)
-            list << li_header( item.name, li_options )
-          else
-            li_content = if include_sub_navigation?(item)
-              if item_container.level == 1
-                if split
-                  main_li_options = li_options.dup
-                  main_li_options[:class] = [ main_li_options[:class], "dropdown-split-left" ].flatten.compact.join(' ')
-                  list << content_tag(:li, simple_link(item, link_options), main_li_options)
-                  li_options[:id] = nil
-                  item.sub_navigation.dom_class = [ item.sub_navigation.dom_class, "pull-right" ].flatten.compact.join(' ')
-                end
-                li_options[:class] = [ li_options[:class], "dropdown", (split ? "dropdown-split-right" : nil) ].flatten.compact.join(' ')
-                dropdown_link(item, split, link_options) + render_sub_navigation_for(item)
-              else
-                li_options[:class] = [ li_options[:class], "dropdown-submenu"].flatten.compact.join(' ')
-                simple_link(item, link_options) + render_sub_navigation_for(item)
-              end
+            navbar_text = li_options.delete(:navbar_text)
+            navbar_divider = li_options.delete(:divider)
+            navbar_header = li_options.delete(:header)
+            link_options = li_options.delete(:link) || {}
+            split = li_options.delete(:split)
+
+            if navbar_text
+              list << li_text( item.name, li_options )
+            elsif navbar_divider
+              list << li_divider( item_container.level, li_options )
+            elsif navbar_header && (item_container.level != 1)
+              list << li_header( item.name, li_options )
             else
-              simple_link(item, link_options)
+              li_content = if include_sub_navigation?(item)
+                if item_container.level == 1
+                  if split
+                    main_li_options = li_options.dup
+                    main_li_options[:class] = [ main_li_options[:class], "dropdown-split-left" ].flatten.compact.join(' ')
+                    list << content_tag(:li, simple_link(item, link_options), main_li_options)
+                    li_options[:id] = nil
+                    item.sub_navigation.dom_class = [ item.sub_navigation.dom_class, "pull-right" ].flatten.compact.join(' ')
+                  end
+                  li_options[:class] = [ li_options[:class], "dropdown", (split ? "dropdown-split-right" : nil) ].flatten.compact.join(' ')
+                  dropdown_link(item, split, link_options) + render_sub_navigation_for(item)
+                else
+                  li_options[:class] = [ li_options[:class], "dropdown-submenu"].flatten.compact.join(' ')
+                  simple_link(item, link_options) + render_sub_navigation_for(item)
+                end
+              else
+                simple_link(item, link_options)
+              end
+              list << content_tag(:li, li_content, li_options)
             end
-            list << content_tag(:li, li_content, li_options)
-          end
-        end.join
+          end.join
 
-        SimpleNavigation.config.selected_class = config_selected_class
-        SimpleNavigation.config.name_generator = @config_name_generator
+        end
 
         item_container.dom_class = [ item_container.dom_class,
                                      (item_container.level == 1) ? "nav#{(options[:bv] == 2) ? '' : ' navbar-nav'}" :
@@ -90,13 +86,27 @@ module SimpleNavigationRenderers
         link_to(link, "#", link_options)
       end
 
-      def name_generator( name )
-        if name.instance_of?(Hash)
-          raise SimpleNavigationRenderers::InvalidHash unless name.keys.include?(:icon) || name.keys.include?(:text)
-          [ (name[:icon] ? content_tag(:span, '', {class: name[:icon]}.merge(name[:title] ? {title: name[:title]} : {}) ) : nil), name[:text] ].compact.join(' ')
-        else
-          @config_name_generator.call(name)
+      def with_bootstrap_configs
+        config_selected_class = SimpleNavigation.config.selected_class
+        config_name_generator = SimpleNavigation.config.name_generator
+
+        SimpleNavigation.config.selected_class = "active"
+        # name_generator should be proc (not lambda or method) to be compatible with earlier versions of simple-navigation
+        SimpleNavigation.config.name_generator = proc do | name, item |
+          if name.instance_of?(Hash)
+            raise SimpleNavigationRenderers::InvalidHash unless name.keys.include?(:icon) || name.keys.include?(:text)
+            [ (name[:icon] ? content_tag(:span, '', {class: name[:icon]}.merge(name[:title] ? {title: name[:title]} : {}) ) : nil), name[:text] ].compact.join(' ')
+          else
+            config_name_generator.call( name, item )
+          end
         end
+
+        result = yield
+
+        SimpleNavigation.config.name_generator = config_name_generator
+        SimpleNavigation.config.selected_class = config_selected_class
+
+        result
       end
 
   end
